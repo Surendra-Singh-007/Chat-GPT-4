de6feb9b6c232b6a9e61ae07f186a6f2cfd9f48f
Surendra Singh <laddu22897@gmail.com>
2023-04-22 16:26:31 +0530

    Files Added

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css'
+import { Navbar, Brand, CTA } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
